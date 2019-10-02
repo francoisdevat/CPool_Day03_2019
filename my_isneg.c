0bbacc71787d46a04c26dee00f6642311486f899
françois de vathaire <fvathaire@localhost.localdomain>
2019-10-02 15:10:54 -0400

    add_of_my_inseg.c

---
+++ my_isneg.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2019
+** fonction positive ou négative
+** File description:
+** je vais tenter celle la 
+*/
+
+void my_putchar(char c);
+
+int my_isneg(int n)
+{
+    if (n >= 0)
+    {
+        my_putchar('P');
+            }
+    else
+    {
+        my_putchar('N');
+            }
+    my_putchar('\n');
+}
+    
+
